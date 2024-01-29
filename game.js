a67a8a87bdaba4ceae14844917e48445037fce43
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-01-29 02:28:15 -0600

    Track diagonals

--- game.js
+++ game.js
@@ -15,25 +15,29 @@ const game = (function() {
             [null, null, null]
         ],
         updateDiagonals: function() {
-
+            board.diagonals = [
+                [board.rows[0][0], board.rows[1][1], board.rows[2][2]],
+                [board.rows[0][2], board.rows[1][1], board.rows[2][0]]
+            ];
         }
     };
-    const allLines = [board.rows, board.columns, board.diagonals];
 
     function markTile(marker, pos) {
         const row = pos[0];
         const col = pos[1];
 
         if (0 <= row < 3 && 0 <= col < 3) {
-            (board.rows[row])[col] = marker;
-            (board.columns[col])[row] = marker;
-            // board.updateDiagonals();
+            board.rows[row][col] = marker;
+            board.columns[col][row] = marker;
+            board.updateDiagonals();
         }
         console.log(game.board);
         // checkWin();
     }
 
     function clearBoard() {
+        const allLines = [board.rows, board.columns, board.diagonals];
+
         allLines.forEach((direction) => {
             direction.forEach((line) => {
                 line.splice(0, 3, null, null, null);
@@ -42,7 +46,9 @@ const game = (function() {
         console.log(game.board);
     }
 
-    return { markTile, clearBoard, board };
+    return {markTile, clearBoard};
 })();
+
+game.markTile("O", [0,2]);
 
 
